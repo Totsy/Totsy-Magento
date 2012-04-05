a297a086c2c33d2e6f16f435ada0c4e418eca42d
Jun Zhao <j.zhao@harapartners.com>
2012-04-05 11:22:13 -0400

    Legacy customer login Minor fix for default fulfillment type

--- app/code/local/Harapartners/Ordersplit/Model/Product/Attribute/Source/Fulfillment.php
+++ app/code/local/Harapartners/Ordersplit/Model/Product/Attribute/Source/Fulfillment.php
@@ -19,16 +19,16 @@ class Harapartners_Ordersplit_Model_Product_Attribute_Source_Fulfillment extends
         	$helper = Mage::helper('ordersplit');
             $this->_options = array(
                 array(
-                    'value' => Harapartners_Ordersplit_Helper_Data::TYPE_VIRTUAL,
-                    'label' => $helper->__('Virtual')
-                ),
-                array(
                     'value' => Harapartners_Ordersplit_Helper_Data::TYPE_DOTCOM,
                     'label' => $helper->__('DotCom')
                 ),
                 array(
                     'value' => Harapartners_Ordersplit_Helper_Data::TYPE_DROPSHIP,
                     'label' => $helper->__('Dropship')
+                ),
+                array(
+                    'value' => Harapartners_Ordersplit_Helper_Data::TYPE_VIRTUAL,
+                    'label' => $helper->__('Virtual')
                 ),
                 array(
                     'value' => Harapartners_Ordersplit_Helper_Data::TYPE_OTHER,
