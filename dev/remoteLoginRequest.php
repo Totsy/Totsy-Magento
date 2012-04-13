1165b94f38cff3d547d6cd78f0289eb30f79890c
andu <a.zhang@harapartners.com>
2012-04-13 17:38:34 -0400

    affiliate

---
+++ dev/remoteLoginRequest.php
@@ -0,0 +1,31 @@
+<?php
+// customer can register through remote affiliate post.
+// response {"status":"failed","error_message":"xxx"}  ;
+//          {"status":"success","login_url":"http://totsy.local.com/affiliate/remote/login?email=xx&password=yyy&product_url=zzz"} 
+// work for both http remost post and parents.com requirement
+// request example follow
+$baseUrlTotsy = 'http://totsy.local.com/affiliate/remote/register';                       //tosty 
+$baseUrlMamasource = 'http://totsy.local.com/mamasource/affiliate/remote/register';       //mamasource
+$email = 'asdasd@asd.com';
+$productUrl = 'product_url';                                                              // parents.com only param
+$affiliateCode = 'parents_com';
+$otherfield = '';                                                                         //subaffiliate_code etc
+$password = 'pwdasda';                                                                        // if null autogenerate
+//totsy mamasource
+$url = $baseUrlTotsy.'?affiliate_code='.$affiliateCode;                                   //work with mamasource too
+$data = array(
+        'email' => $email,
+        'password' => $password,
+        'product_url' => $productUrl,
+		'other_field'=> $otherfield
+         );
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt ( $ch, CURLOPT_URL, $url );
+curl_setopt ( $ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+$result = curl_exec ( $ch );
+curl_close ( $ch );
+foreach (json_decode($result,true) as $index=>$value) {
+	echo $index.': '.$value.'<br/>';
+}
