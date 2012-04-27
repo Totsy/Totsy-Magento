5efda9935ded63152fa4fc89eb6b1510b6f876d7
yang li <y.li@harapartners.com>
2012-04-27 19:41:37 -0400

    Resolving cache contamination between web and mobile skin

--- app/code/local/Harapartners/Categoryevent/Model/Cache/Topnav.php
+++ app/code/local/Harapartners/Categoryevent/Model/Cache/Topnav.php
@@ -4,36 +4,20 @@ class Harapartners_Categoryevent_Model_Cache_Topnav extends Enterprise_PageCache
 {
     const CACHE_TAG_PREFIX = 'catagoryevent_topnav';
 
-    /**
-     * Get identifier from cookies
-     *
-     * @return string
-     */
-    protected function _getIdentifier()
-    {
-        return '';
+    protected function _getIdentifier() {
+        $params = Mage::registry('application_params');
+        $scopeCode = '';
+        if(isset($params['scope_code'])) {
+            $scopeCode = $params['scope_code'];
+        }
+        return $scopeCode;
     }
-	
-//    protected function _getAdditionalCacheId(){
-//    	return md5('CONTAINER_EVENT_INDEX_TOPNAV_' . $this->_placeholder->getAttribute('cache_id'));
-//    }
-    /**
-     * Get cache identifier
-     *
-     * @return string
-     */
-    protected function _getCacheId()
-    {
+    
+    protected function _getCacheId() {
         return md5(self::CACHE_TAG_PREFIX . $this->_getIdentifier());
     }
 
-    /**
-     * Render block content
-     *
-     * @return string
-     */
-    protected function _renderBlock()
-    {
+    protected function _renderBlock() {
         $block = $this->_placeholder->getAttribute('block');
         //you can use a hard coded template here like xxxx_cached.phtml
         $template = $this->_placeholder->getAttribute('template');
@@ -41,8 +25,6 @@ class Harapartners_Categoryevent_Model_Cache_Topnav extends Enterprise_PageCache
         $block->setNameInLayout('categoryevent');
         $block->setTemplate($template);
         $block->setLayout(Mage::app()->getLayout());
-
-
         return $block->toHtml();
     }
 
