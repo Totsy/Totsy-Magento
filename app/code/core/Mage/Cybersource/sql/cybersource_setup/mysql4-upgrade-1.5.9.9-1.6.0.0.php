ae93898285f42ea71d3c264ff6475b68aa13364f
troyer <royertom.fr@gmail.com>
2012-05-10 12:10:30 -0400

    Revert to d2f733e4e2edf36202d7264197e1efe11ac16bd4

---
+++ app/code/core/Mage/Cybersource/sql/cybersource_setup/mysql4-upgrade-1.5.9.9-1.6.0.0.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Cybersource
+ * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/* @var $installer Mage_Cybersource_Model_Resource_Setup */
+$installer = $this;
+$installer->startSetup();
+
+/**
+ * Change columns
+ */
+$tables = array(
+    $installer->getTable('sales/quote_payment') => array(
+        'columns' => array(
+            'cybersource_token' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => 255,
+                'comment'   => 'Cybersource Token'
+            )
+        )
+    ),
+    $installer->getTable('sales/quote_payment') => array(
+        'columns' => array(
+            'cybersource_subid' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => 255,
+                'comment'   => 'Cybersource SubID'
+            )
+        )
+    ),    
+    $installer->getTable('sales/order_payment') => array(
+        'columns' => array(
+            'cybersource_token' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => 255,
+                'comment'   => 'Cybersource Token'
+            )
+        )
+    ),
+    $installer->getTable('sales/order_payment') => array(
+        'columns' => array(
+            'cybersource_subid' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => 255,
+                'comment'   => 'Cybersource SubID'
+            )
+        )
+    ),    
+    $installer->getTable('sales/invoice') => array(
+        'columns' => array(
+            'cybersource_token' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => 255,
+                'comment'   => 'Cybersource Token'
+            )
+        )
+    ),
+    $installer->getTable('sales/invoice') => array(
+        'columns' => array(
+            'cybersource_subid' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => 255,
+                'comment'   => 'Cybersource SubID'
+            )
+        )
+    ),
+    $installer->getTable('sales/creditmemo') => array(
+        'columns' => array(
+            'cybersource_token' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => 255,
+                'comment'   => 'Cybersource Token'
+            )
+        )
+    ),
+     $installer->getTable('sales/creditmemo') => array(
+        'columns' => array(
+            'cybersource_subid' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => 255,
+                'comment'   => 'Cybersource SubID'
+            )
+        )
+    ),
+);
+
+$installer->getConnection()->modifyTables($tables);
+
+$installer->endSetup();
