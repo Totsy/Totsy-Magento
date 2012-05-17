4d22f40354ff524c13c9a4cc849623f33d58a1f5
Edward Leong <e.leong@harapartners.com>
2012-05-17 16:12:31 -0400

    commit

--- app/code/community/Inchoo/Facebook/controllers/Customer/AccountController.php
+++ app/code/community/Inchoo/Facebook/controllers/Customer/AccountController.php
@@ -4,7 +4,7 @@
  *
  * @category    Inchoo
  * @package     Inchoo_Facebook
- * @author      Ivan Weiler <ivan.weiler@gmail.com>
+ * @author      Ivan Weiler  <ivan.weiler@gmail.com>
  * @copyright   Inchoo (http://inchoo.net)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
