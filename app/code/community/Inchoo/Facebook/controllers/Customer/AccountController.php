4733a65930dd558af1bc50bd3d6d0caeb5ddaf8c
Edward Leong <e.leong@harapartners.com>
2012-05-17 16:11:41 -0400

    reopen validate incase directly type url

--- app/code/community/Inchoo/Facebook/controllers/Customer/AccountController.php
+++ app/code/community/Inchoo/Facebook/controllers/Customer/AccountController.php
@@ -31,11 +31,11 @@ class Inchoo_Facebook_Customer_AccountController extends Mage_Core_Controller_Fr
 
     public function connectAction()
     {
-        /*if(!$this->_getSession()->validate()) {
+        if(!$this->_getSession()->validate()) {
             $this->_getCustomerSession()->addError($this->__('Facebook connection failed.'));
             $this->_redirect('customer/account/login');
             return;
-        }*/
+        }
 
         //login or connect
 
