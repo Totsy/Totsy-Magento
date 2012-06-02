084c072be4a1a4507eec62ca6df4d3a428137c4b
Jun Zhao <j.zhao@harapartners.com>
2012-06-02 11:18:49 -0400

    Cart cleaning (cart reservation) updates

--- app/code/local/Harapartners/Rushcheckout/Model/Observer.php
+++ app/code/local/Harapartners/Rushcheckout/Model/Observer.php
@@ -18,6 +18,8 @@ class Harapartners_Rushcheckout_Model_Observer {
     
     const CUSTOMER_VALIDATION_CHECK_URL = 'customer/revalidate/index';
     const CUSTOMER_REVALIDATE_TIMER_LIMIT = 3600;
+    const DEFAULT_COLLECTION_SIZE_LIMIT = 10; //in case the collection is rather large
+    
     /**
      * Double validation check HP
      */
@@ -92,27 +94,58 @@ class Harapartners_Rushcheckout_Model_Observer {
     // ===== Cronjob related ===== //
     public function cleanExpiredQuotes() {
         $itemCount = 0;
+        $itemQtyCount = 0;
         $cartCount = 0;
         $lifetimes = Mage::getConfig()->getStoresConfigByPath('config/rushcheckout_timer/limit_timer');
+        
+        //Separate for different stores
         foreach ($lifetimes as $storeId => $lifetime) {
-            $quoteCollection = Mage::getModel('sales/quote')->getCollection();
-            $quoteCollection->addFieldToFilter('store_id', $storeId);
-            $quoteCollection->addFieldToFilter('is_active', 1);
-            $quoteCollection->addFieldToFilter('updated_at', array('to' => date("Y-m-d H:i:s", time() - $lifetime)));
-            foreach($quoteCollection as $quote){
-                foreach($quote->getAllItems() as $item){
-                    $item->isDeleted(true);
-                    $item->delete();
-
-                    $itemCount++;
-                }
-
-                $cartCount++;
-            }
+        	//Optimization for large collections, since sales/quote is a flat table, this is relatively efficient
+        	//Entity ID offset is safer than LIMIT clause offset, this would safe guard against endless loop
+        	//LIMIT clause offset is vulnerable if other processes also update the table
+        	$entityIdOffset = 0;
+        	do{
+	            $quoteCollection = Mage::getModel('sales/quote')->getCollection();
+	            $quoteCollection->addFieldToFilter('store_id', $storeId)
+						->addFieldToFilter('entity_id', array('gt' => $entityIdOffset))
+	            		->addFieldToFilter('items_count', array('gt' => 0))
+	            		->addFieldToFilter('updated_at', array('to' => date("Y-m-d H:i:s", time() - $lifetime)));
+            	$quoteCollection->getSelect()
+            			->limit(self::DEFAULT_COLLECTION_SIZE_LIMIT)
+            			->order('entity_id ASC');
+	            foreach($quoteCollection as $quote){
+	                foreach($quote->getAllItems() as $item){
+	                    $item->isDeleted(true);
+	                    $item->delete(); //Cart reservation logic: item qty is release back to available by observer
+	                    
+	                    //Avoid double counting parent/children items
+	                    if(!$item->getParentItemId()){
+	                    	$itemQtyCount += $item->getQty(); 
+	                    	$itemCount++;
+	                    }
+	                    
+	                    unset($item);
+	                }
+	                $entityIdOffset = $quote->getId();
+	                $quote->setData('items_count', 0)
+	                		->setData('items_qty', 0)
+	                		->setData('grand_total', 0)
+	                		->setData('base_grand_total', 0)
+	                		->setData('subtotal', 0)
+	                		->setData('base_subtotal', 0)
+	                		->setData('subtotal_with_discount', 0)
+	                		->setData('base_subtotal_with_discount', 0)
+	                		->save();
+	                $cartCount++;
+	                unset($quote);
+	            }
+	            $collectionSize = count($quoteCollection);
+	            unset($quoteCollection);
+            }while($collectionSize >= self::DEFAULT_COLLECTION_SIZE_LIMIT);
         }
 
         Mage::log(
-            sprintf('Pruned %d items out of %d carts.', $itemCount, $cartCount),
+            sprintf('Pruned %d line items (%d total qty) out of %d carts.', $itemCount, $itemQtyCount, $cartCount),
             Zend_Log::INFO,
             'cart_cleaner.log'
         );
