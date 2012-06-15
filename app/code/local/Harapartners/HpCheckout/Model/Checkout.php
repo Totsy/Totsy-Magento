573b12eadd6c219d80528b4f005b46021d057208
eubiera <eubiera@totsy.com>
2012-06-15 18:25:15 -0400

    starting virtual products email

--- app/code/local/Harapartners/HpCheckout/Model/Checkout.php
+++ app/code/local/Harapartners/HpCheckout/Model/Checkout.php
@@ -10,35 +10,35 @@
  * to eula@harapartners.com so we can send you a copy immediately.
  *
  */
-class Harapartners_HpCheckout_Model_Checkout 
+class Harapartners_HpCheckout_Model_Checkout
 {
     const METHOD_GUEST = 'guest';
     const METHOD_CUSTOMER = 'customer';
-    
+
     protected $_quote;
     protected $_checkoutSession;
     protected $_customerSession;
     protected $_helper;
-    
+
     public function __construct() {
         $this->_checkoutSession = Mage::getSingleton('checkout/session');
         $this->_customerSession = Mage::getSingleton('customer/session');
         $this->_quote = $this->_checkoutSession->getQuote();
         $this->_helper = Mage::helper( 'checkout' );
     }
-    
+
     public function getCheckout() {
         return $this->_checkoutSession;
     }
-    
+
     public function getCustomerSession() {
         return $this->_customerSession;
     }
-    
+
     public function getQuote() {
         return $this->_quote;
     }
-    
+
     public function saveBilling( $data )
     {
         if( empty( $data ) ) {
@@ -48,12 +48,12 @@ class Harapartners_HpCheckout_Model_Checkout
         $address = $this->getQuote()->getBillingAddress();
         $addressForm = Mage::getModel( 'customer/form' );
         $addressForm->setFormCode( 'customer_address_edit' )
-            ->setEntityType( 'customer_address' )
-            ->setIsAjaxRequest(Mage::app()->getRequest()->isAjax());
+        ->setEntityType( 'customer_address' )
+        ->setIsAjaxRequest(Mage::app()->getRequest()->isAjax());
 
         $addressForm->setEntity($address);
         $addressData = $addressForm->extractData( $addressForm->prepareRequest( $data ) );
-        
+
         $addressErrors = $addressForm->validateData( $addressData );
         if( $addressErrors !== true ) {
             return array( 'status' => 1, 'message' => $addressErrors );
@@ -66,7 +66,7 @@ class Harapartners_HpCheckout_Model_Checkout
         }
 
         $address->setData( 'email', $data[ 'email' ] );
-        
+
         if (($validateRes = $address->validate()) !== true) {
             return array('status' => 1, 'message' => $validateRes);
         }
@@ -77,16 +77,16 @@ class Harapartners_HpCheckout_Model_Checkout
             return $result;
         }
 
-//        $this->getQuote()->collectTotals();
-//        $this->getQuote()->save();
+        //        $this->getQuote()->collectTotals();
+        //        $this->getQuote()->save();
         return array( 'status' => 0, 'message' => '' );
     }
-    
+
     protected function _validateCustomerData( array $data )
     {
         $customerForm = Mage::getModel('customer/form');
         $customerForm->setFormCode('checkout_register')
-            ->setIsAjaxRequest(Mage::app()->getRequest()->isAjax());
+        ->setIsAjaxRequest(Mage::app()->getRequest()->isAjax());
 
         $quote = $this->getQuote();
         if ( $quote->getCustomerId() ) {
@@ -130,7 +130,7 @@ class Harapartners_HpCheckout_Model_Checkout
         Mage::helper('core')->copyFieldset('customer_account', 'to_quote', $customer, $quote);
         return true;
     }
-    
+
     public function saveShipping( $data )
     {
         if (empty($data)) {
@@ -140,8 +140,8 @@ class Harapartners_HpCheckout_Model_Checkout
 
         $addressForm    = Mage::getModel('customer/form');
         $addressForm->setFormCode('customer_address_edit')
-            ->setEntityType('customer_address')
-            ->setIsAjaxRequest(Mage::app()->getRequest()->isAjax());
+        ->setEntityType('customer_address')
+        ->setIsAjaxRequest(Mage::app()->getRequest()->isAjax());
 
         $addressForm->setEntity($address);
         $addressData    = $addressForm->extractData($addressForm->prepareRequest($data));
@@ -165,11 +165,11 @@ class Harapartners_HpCheckout_Model_Checkout
             return array('status' => 1, 'message' => $validateRes);
         }
 
-//        $this->getQuote()->collectTotals()->save();
+        //        $this->getQuote()->collectTotals()->save();
 
         return array( 'status' => 0, 'message' => '' );
     }
-    
+
     public function saveShippingMethod( $shippingMethod )
     {
         if (empty($shippingMethod)) {
@@ -180,13 +180,13 @@ class Harapartners_HpCheckout_Model_Checkout
             return array('status' => -1, 'message' => $this->_helper->__('Invalid shipping method.'));
         }
         $this->getQuote()->getShippingAddress()
-            ->setShippingMethod($shippingMethod);
-//        $this->getQuote()->collectTotals()
-//            ->save();
+        ->setShippingMethod($shippingMethod);
+        //        $this->getQuote()->collectTotals()
+        //            ->save();
 
         return array( 'status' => 0, 'message' => '' );
     }
-    
+
     public function savePayment( $data, $shouldCollectTotal = true, $withValidate = true )
     {
         if (empty($data)) {
@@ -202,36 +202,37 @@ class Harapartners_HpCheckout_Model_Checkout
         if (!$quote->isVirtual() && $quote->getShippingAddress()) {
             $quote->getShippingAddress()->setCollectShippingRates(true);
         }
-        
-        
+
+
         $payment = $quote->getPayment();
         $payment->importData($data, $shouldCollectTotal, $withValidate);
 
-//        $quote->save();
+        //        $quote->save();
 
         return array( 'status' => 0, 'message' => '' );
     }
-    
+
     public function saveOrder()
     {
         $this->validate();
         switch ($this->getCheckoutMethod()) {
-            case self::METHOD_GUEST:
-                $this->_prepareGuestQuote();
-                break;
-            default:
-                $this->_prepareCustomerQuote();
-                break;
+        case self::METHOD_GUEST:
+            $this->_prepareGuestQuote();
+            break;
+        default:
+            $this->_prepareCustomerQuote();
+            break;
         }
-        
+
         $service = Mage::getModel('sales/service_quote', $this->getQuote());
         $service->submitAll();
 
         $this->_checkoutSession->setLastQuoteId($this->getQuote()->getId())
-            ->setLastSuccessQuoteId($this->getQuote()->getId())
-            ->clearHelperData();
+        ->setLastSuccessQuoteId($this->getQuote()->getId())
+        ->clearHelperData();
 
         $order = $service->getOrder();
+
         if ($order) {
             Mage::dispatchEvent('hpcheckout_save_order_after',
                 array('order'=>$order, 'quote'=>$this->getQuote()));
@@ -239,16 +240,56 @@ class Harapartners_HpCheckout_Model_Checkout
             $redirectUrl = $this->getQuote()->getPayment()->getOrderPlaceRedirectUrl();
 
             if (!$redirectUrl && $order->getCanSendNewEmailFlag()) {
-                try {
+                try {    
+                
+                    foreach($order->getAllItems() as $orderItem){
+                        
+                        if($orderItem->getProductType() == 'virtual'){
+
+                            $virtualProductCode = $orderItem->getProductOptionByCode('reservation_code');
+                            
+                            print $virtualProductCode;
+                            
+                            /*
+                            
+                            print $virtualProductCode;
+                            exit();
+                            
+                            $templateId = 15;
+
+                            $mailer = Mage::getModel('core/email_template_mailer');
+                            $emailInfo = Mage::getModel('core/email_info');
+                            $customer = Mage::getModel('customer/customer');
+
+                            $emailInfo->addTo($customer->getCustomerEmail(), $customer->getCustomerName());
+
+                            $mailer->addEmailInfo($emailInfo);
+                            //$mailer->setSender(Mage::getStoreConfig(XML_PATH_EMAIL_IDENTITY, $customer['store_id']));
+                            $mailer->setStoreId($customer['store_id']);
+                            $mailer->setTemplateId($templateId);
+                            $mailer->setTemplateParams( array(
+                                    'order'        => $order,
+                                    'store'        => Mage::app()->getStore(),
+                                    'store_view'=>$store = Mage::app()->getStore()->getStoreView(),
+                                    'virtual_product_code'=> $virtualProductCode
+                                )
+                            );
+                            $mailer->send();
+                            */
+                        } 
+                    }
+
                     $order->sendNewOrderEmail();
+                    exit();
+                    
                 } catch (Exception $e) {
                     Mage::logException($e);
                 }
             }
 
             $this->_checkoutSession->setLastOrderId($order->getId())
-                ->setRedirectUrl($redirectUrl)
-                ->setLastRealOrderId($order->getIncrementId());
+            ->setRedirectUrl($redirectUrl)
+            ->setLastRealOrderId($order->getIncrementId());
         }
 
         $profiles = $service->getRecurringPaymentProfiles();
@@ -259,7 +300,7 @@ class Harapartners_HpCheckout_Model_Checkout
 
         return $this;
     }
-    
+
     public function validate()
     {
         $helper = Mage::helper('checkout');
@@ -269,17 +310,17 @@ class Harapartners_HpCheckout_Model_Checkout
             Mage::throwException($this->_helper->__('Sorry, guest checkout is not enabled. Please try again or contact store owner.'));
         }
     }
-    
+
     protected function _prepareGuestQuote()
     {
         $quote = $this->getQuote();
         $quote->setCustomerId(null)
-            ->setCustomerEmail($quote->getBillingAddress()->getEmail())
-            ->setCustomerIsGuest(true)
-            ->setCustomerGroupId(Mage_Customer_Model_Group::NOT_LOGGED_IN_ID);
+        ->setCustomerEmail($quote->getBillingAddress()->getEmail())
+        ->setCustomerIsGuest(true)
+        ->setCustomerGroupId(Mage_Customer_Model_Group::NOT_LOGGED_IN_ID);
         return $this;
     }
-    
+
     protected function _prepareCustomerQuote()
     {
         $quote      = $this->getQuote();
@@ -305,11 +346,11 @@ class Harapartners_HpCheckout_Model_Checkout
         if ($shipping && isset($customerShipping) && !$customer->getDefaultShipping()) {
             $customerShipping->setIsDefaultShipping(true);
         } else if (isset($customerBilling) && !$customer->getDefaultShipping()) {
-            $customerBilling->setIsDefaultShipping(true);
-        }
+                $customerBilling->setIsDefaultShipping(true);
+            }
         $quote->setCustomer($customer);
     }
-    
+
     public function getCheckoutMethod()
     {
         if ($this->getCustomerSession()->isLoggedIn()) {
@@ -320,5 +361,5 @@ class Harapartners_HpCheckout_Model_Checkout
         }
         return $this->getQuote()->getCheckoutMethod();
     }
-    
+
 }
--- app/code/local/Harapartners/HpCheckout/controllers/CheckoutController.php
+++ app/code/local/Harapartners/HpCheckout/controllers/CheckoutController.php
@@ -1,38 +1,38 @@
 <?php
 class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controller_Action {
-    
-    protected $_stepControl = array( 
-                                        array( 'billing' => false ), 
-                                         array( 'shipping' => false ), 
-                                         array( 'shipping_method' => false ), 
-                                         array( 'payment' => false ), 
-                                        array( 'review' => false ) 
-                                    );
-    
+
+    protected $_stepControl = array(
+        array( 'billing' => false ),
+        array( 'shipping' => false ),
+        array( 'shipping_method' => false ),
+        array( 'payment' => false ),
+        array( 'review' => false )
+    );
+
     protected $_order;
-    
-//    public function preDispatch(){
-//        $this->getRequest()->setRoutingInfo(array(
-//                'requested_route' => 'hpcheckout', 
-//                'requested_controller' => 'checkout'
-//        ));
-//        parent::preDispatch();
-//        return $this;
-//    }
-                                    
+
+    //    public function preDispatch(){
+    //        $this->getRequest()->setRoutingInfo(array(
+    //                'requested_route' => 'hpcheckout',
+    //                'requested_controller' => 'checkout'
+    //        ));
+    //        parent::preDispatch();
+    //        return $this;
+    //    }
+
     public function indexAction() {
         $this->validateCart();
         $this->loadLayout();
         $this->_initLayoutMessages( 'checkout/session' );
         $this->_resetCartTimer();
-        $this->renderLayout();                                
+        $this->renderLayout();
     }
-    
+
     public function updateAction() {
         $jsonArray = array();
         $postData = $this->getRequest()->getPost();
         try{
-        	$jsonArray = $this->_getBlocksArray( $postData );
+            $jsonArray = $this->_getBlocksArray( $postData );
         } catch (Mage_Core_Exception $e) {
             $jsonArray['status'] = 1;
             $jsonArray['message'] = $e->getMessage();
@@ -42,12 +42,12 @@ class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controlle
         }
         $this->getResponse()->setBody( Mage::helper( 'core' )->jsonEncode( $jsonArray ) );
     }
-    
+
     public function submitAction() {
         $this->validateCart();
         $result = array();
         $customer = Mage::getSingleton('customer/session')->getCustomer();
-        $customerId = $customer->getId(); 
+        $customerId = $customer->getId();
         try {
             $blocksSuccessFlag = true;
             $postData = $this->getRequest()->getPost();
@@ -74,21 +74,25 @@ class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controlle
                     }
                     $this->_getHpCheckout()->getQuote()->getPayment()->importData($data);
                 }
+
+                $service = Mage::getModel('sales/service_quote', $this->_getHpCheckout()->getQuote());
+                $order = $service->getQuote();
+                
                 $this->_getHpCheckout()->saveOrder();
-    
+
                 $storeId = Mage::app()->getStore()->getId();
                 $paymentHelper = Mage::helper("payment");
                 $zeroSubTotalPaymentAction = $paymentHelper->getZeroSubTotalPaymentAutomaticInvoice($storeId);
                 if ($paymentHelper->isZeroSubTotal($storeId)
-                        && $this->_getOrder()->getGrandTotal() == 0
-                        && $zeroSubTotalPaymentAction == Mage_Payment_Model_Method_Abstract::ACTION_AUTHORIZE_CAPTURE
-                        && $paymentHelper->getZeroSubTotalOrderStatus($storeId) == 'pending') {
+                    && $this->_getOrder()->getGrandTotal() == 0
+                    && $zeroSubTotalPaymentAction == Mage_Payment_Model_Method_Abstract::ACTION_AUTHORIZE_CAPTURE
+                    && $paymentHelper->getZeroSubTotalOrderStatus($storeId) == 'pending') {
                     $invoice = $this->_initInvoice();
                     $invoice->getOrder()->setIsInProcess(true);
                     $invoice->save();
                 }
-            
-//                $redirectUrl = $this->_getHpCheckout()->getCheckout()->getRedirectUrl();
+
+                //                $redirectUrl = $this->_getHpCheckout()->getCheckout()->getRedirectUrl();
                 $result['status'] = 0;
             }
         } catch (Mage_Payment_Model_Info_Exception $e) {
@@ -105,19 +109,19 @@ class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controlle
             $result['message'] = $this->__('There was an error processing your order. Please contact us or try again later.');
         }
         $this->_getHpCheckout()->getQuote()->save();
-//        if (isset($redirectUrl)) {
-//            $result['redirect'] = $redirectUrl;
-//        }
+        //        if (isset($redirectUrl)) {
+        //            $result['redirect'] = $redirectUrl;
+        //        }
 
         $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($result));
     }
-    
+
     public function successAction() {
         $this->loadLayout();
         $this->_initLayoutMessages( 'checkout/session' );
         $this->renderLayout();
     }
-    
+
     public function validateCart() {
         $quote = $this->_getHpCheckout()->getQuote();
         if (!$quote->hasItems() || $quote->getHasError()) {
@@ -131,41 +135,41 @@ class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controlle
             return;
         }
         Mage::getSingleton('checkout/session')->setCartWasUpdated(false);
-        Mage::getSingleton('customer/session')->setBeforeAuthUrl(Mage::getUrl('*/*/*', array('_secure'=>true)));    
+        Mage::getSingleton('customer/session')->setBeforeAuthUrl(Mage::getUrl('*/*/*', array('_secure'=>true)));
     }
-    
+
     public function collectTotals() {
         $this->_getHpCheckout()->getQuote()->collectTotals()->save();
     }
-    
+
     protected function _getBlocksArray( $postData, $isSubmit = false ) {
         $json = array();
         $currentStep = /*isset( $postData[ 'currentStep' ] ) ? $postData[ 'currentStep' ] : */'billing';
-        
+
         $this->_updateBlocks( $postData );
         foreach( $this->_stepControl as $step ) {
             foreach( $step as $blockCode => $flag ) {
                 if( $flag ) {
                     switch( $blockCode ) {
-                        case 'billing':
-                            $json[ 'billing' ] = $this->_updateBilling( isset( $postData[ 'billing' ] ) ? $postData[ 'billing' ] : array(), isset( $postData[ 'billing_address_id' ] ) ? $postData[ 'billing_address_id' ] : false );
-                            break;
-                        case 'shipping':
-                            if( ! $this->_getHpCheckout()->getQuote()->isVirtual() ) {
-                                $json[ 'shipping' ] = $this->_updateShipping( isset( $postData[ 'shipping' ] ) ? $postData[ 'shipping' ] : array() );
-                            }
-                            break;
-                        case 'shipping_method':
-                            if( ! $this->_getHpCheckout()->getQuote()->isVirtual() ) {
-                                $json[ 'shipping_method' ] = $this->_updateShippingMethod( isset( $postData[ 'shipping_method' ] ) ? $postData[ 'shipping_method' ] : '' );
-                            }
-                            break;
-                        case 'payment':
-                            $json[ 'payment' ] = $this->_updatePayment( isset( $postData[ 'payment' ] ) ? $postData[ 'payment' ] : array(), true, $isSubmit );
-                            break;
-                        case 'review':
-                            $json[ 'review' ] = $this->_updateReview();
-                            break;
+                    case 'billing':
+                        $json[ 'billing' ] = $this->_updateBilling( isset( $postData[ 'billing' ] ) ? $postData[ 'billing' ] : array(), isset( $postData[ 'billing_address_id' ] ) ? $postData[ 'billing_address_id' ] : false );
+                        break;
+                    case 'shipping':
+                        if( ! $this->_getHpCheckout()->getQuote()->isVirtual() ) {
+                            $json[ 'shipping' ] = $this->_updateShipping( isset( $postData[ 'shipping' ] ) ? $postData[ 'shipping' ] : array() );
+                        }
+                        break;
+                    case 'shipping_method':
+                        if( ! $this->_getHpCheckout()->getQuote()->isVirtual() ) {
+                            $json[ 'shipping_method' ] = $this->_updateShippingMethod( isset( $postData[ 'shipping_method' ] ) ? $postData[ 'shipping_method' ] : '' );
+                        }
+                        break;
+                    case 'payment':
+                        $json[ 'payment' ] = $this->_updatePayment( isset( $postData[ 'payment' ] ) ? $postData[ 'payment' ] : array(), true, $isSubmit );
+                        break;
+                    case 'review':
+                        $json[ 'review' ] = $this->_updateReview();
+                        break;
                     }
                 }
             }
@@ -180,7 +184,7 @@ class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controlle
         $json[ 'review' ][ 'html' ] = $this->_renderReview();
         return $json;
     }
-    
+
     protected function _getOrder()
     {
         if (is_null($this->_order)) {
@@ -191,7 +195,7 @@ class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controlle
         }
         return $this->_order;
     }
-    
+
     protected function _updateBlocks( $postData ) {
         $currentStep = /*isset( $postData[ 'currentStep' ] ) ? $postData[ 'currentStep' ] : */'billing';
         $updatePayment = isset( $postData[ 'updatePayment' ] ) ? $postData[ 'updatePayment' ] : false;
@@ -206,7 +210,7 @@ class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controlle
         }
         $this->_stepControl[ 3 ][ 'payment' ] = $updatePayment ? true : false;
     }
-    
+
     protected function _updateBilling( $billingData = array() )
     {
         $result = array();
@@ -218,8 +222,8 @@ class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controlle
         }
         return $result;
     }
-    
-    protected function _updateShipping( $shippingData = array() ) 
+
+    protected function _updateShipping( $shippingData = array() )
     {
         $result = array();
         if( ! empty( $shippingData ) ) {
@@ -227,7 +231,7 @@ class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controlle
         }
         return $result;
     }
-    
+
     protected function _updateShippingMethod( $shippingMethodData = '' )
     {
         $result = array();
@@ -235,10 +239,10 @@ class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controlle
         if( $trueShippingMethodData ) {
             $result = $this->_getHpCheckout()->saveShippingMethod($trueShippingMethodData);
         }
-        
+
         return $result;
     }
-    
+
     protected function _validateShippingMethod($shippingMethodData){
         $allowedMethods = array();
         $shippingAddress = $this->_getHpCheckout()->getQuote()->getShippingAddress();
@@ -257,10 +261,10 @@ class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controlle
                 return $allowedMethods[0];
             }
         }
-        
+
         return '';
     }
-    
+
     protected function _updatePayment( $paymentData, $shouldCollectTotal = true, $withValidate = true ) {
         try {
             $result = array();
@@ -273,84 +277,84 @@ class Harapartners_HpCheckout_CheckoutController extends Mage_Checkout_Controlle
         }
         return $result;
     }
-    
+
     protected function _updateReview() {
         return array( 'status' => 0, 'message' => '' );
     }
-    
+
     protected function _renderBilling() {
         return $this->getLayout()->getBlock( 'hpcheckout.billing' )->toHtml();
     }
-    
+
     protected function _renderShipping() {
         return $this->getLayout()->getBlock( 'hpcheckout.shipping' )->toHtml();
     }
-    
+
     protected function _renderShippingMethod() {
         return $this->getLayout()->getBlock( 'hpcheckout.shipping_method' )->toHtml();
     }
-    
+
     protected function _renderPayment() {
         return $this->getLayout()->getBlock( 'hpcheckout.payment' )->toHtml();
     }
-    
+
     protected function _renderReview() {
         return $this->getLayout()->getBlock( 'hpcheckout.review' )->toHtml();
     }
-    
+
     protected function _getHpCheckout() {
         return Mage::getSingleton( 'hpcheckout/checkout' );
     }
-    
+
     protected function _getSession() {
         return Mage::getSingleton('customer/session');
     }
-    
+
     protected function _resetCartTimer(){
-    	$helper = Mage::helper('hpcheckout');
-    	$checkoutSession = Mage::getSingleton('checkout/session');
-    	$checkoutSession->setCountDownTimer($helper->getCurrentTime());    //Harapartners, yang, set new cart timer
-    	return $this;
+        $helper = Mage::helper('hpcheckout');
+        $checkoutSession = Mage::getSingleton('checkout/session');
+        $checkoutSession->setCountDownTimer($helper->getCurrentTime());    //Harapartners, yang, set new cart timer
+        return $this;
     }
     /**
      * Action predispatch
      *
      * Check customer authentication for some actions
      */
-//    public function preDispatch() {
-//        // a brute-force protection here would be nice
-//
-//        parent::preDispatch();
-//
-//        if (!$this->getRequest()->isDispatched()) {
-//            return;
-//        }
-//
-//        $action = $this->getRequest()->getActionName();
-//        $openActions = array(
-//            'validationcheck',
-//            'validationCheckPostAction'
-//        );
-//        $pattern = '/^(' . implode('|', $openActions) . ')/i';
-//        
-//        $session = $this->_getSession();
-//        $session->setBeforeCartAuthUrl(Mage::helper('core/url')->getCurrentUrl());
-//        
-//        if (!preg_match($pattern, $action)) {
-//            if (!$session->authenticate($this, null, true)) {
-//                $this->setFlag('', 'no-dispatch', true);
-//            }
-//        } else {
-//            $this->_getSession()->setNoReferer(true);
-//        }
-//    }
-//    
-//    public function postDispatch() {
-//        parent::postDispatch();
-//        $session = $this->_getSession();
-//        //HP Yang
-//        $session->setBeforeCartAuthUrl('');
-//        //End
-//        $session->unsNoReferer(false);
-//    }
+    //    public function preDispatch() {
+    //        // a brute-force protection here would be nice
+    //
+    //        parent::preDispatch();
+    //
+    //        if (!$this->getRequest()->isDispatched()) {
+    //            return;
+    //        }
+    //
+    //        $action = $this->getRequest()->getActionName();
+    //        $openActions = array(
+    //            'validationcheck',
+    //            'validationCheckPostAction'
+    //        );
+    //        $pattern = '/^(' . implode('|', $openActions) . ')/i';
+    //
+    //        $session = $this->_getSession();
+    //        $session->setBeforeCartAuthUrl(Mage::helper('core/url')->getCurrentUrl());
+    //
+    //        if (!preg_match($pattern, $action)) {
+    //            if (!$session->authenticate($this, null, true)) {
+    //                $this->setFlag('', 'no-dispatch', true);
+    //            }
+    //        } else {
+    //            $this->_getSession()->setNoReferer(true);
+    //        }
+    //    }
+    //
+    //    public function postDispatch() {
+    //        parent::postDispatch();
+    //        $session = $this->_getSession();
+    //        //HP Yang
+    //        $session->setBeforeCartAuthUrl('');
+    //        //End
+    //        $session->unsNoReferer(false);
+    //    }
 }
