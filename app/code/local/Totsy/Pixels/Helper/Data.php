8b2cc18e66a4548705adca1e533dea86eefedcf5
Evan Ubiera <evanubiera@gmail.com>
2012-08-01 01:39:36 +0000

    Adding HTML content stripper for description data in SL posts to Facebook

--- app/code/local/Totsy/Pixels/Helper/Data.php
+++ app/code/local/Totsy/Pixels/Helper/Data.php
@@ -15,4 +15,30 @@ class Totsy_Pixels_Helper_Data
     	}
     	return $_tags;
     }
+    
+    //for strippping data presented in Sociable Labs posts on Facebook
+    public function stripHTMLContent($_htmlString) {
+    	$_dom = new DOMDocument();
+    	
+    	if($_htmlString) {
+            $_dom->loadHTML($_htmlString);
+            $_dom->preserveWhiteSpace = false;
+            
+            //more elements/tags may be stripped out using this type
+            $_elements = $_dom->getElementsByTagName('ul');
+            $_tags = array();
+            
+            foreach($_elements as $_tag) {
+                $_tags[] = $_tag;
+            }
+            
+            foreach($_tags as $_tag) {
+                $_tag->parentNode->removeChild($_tag);
+            }
+            
+            return $_dom->saveHTML();
+        } else {
+        	return "Please enter a value";
+        }
+    }
 }
