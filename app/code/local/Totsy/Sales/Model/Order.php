d6f630e41c8545061a367ae6ae7d7c8931e3f3d4
troyer <troyer@totsy.com>
2012-08-03 21:24:19 +0000

    MGN-817 Update an Order with Credits will set status to Updated

--- app/code/local/Totsy/Sales/Model/Order.php
+++ app/code/local/Totsy/Sales/Model/Order.php
@@ -213,4 +213,44 @@ class Totsy_Sales_Model_Order extends Mage_Sales_Model_Order
         $this->setData('profit', (float) $profit);
         return $this->getData('profit');
     }
+    
+    /**
+     * Check order state before saving
+     */
+    protected function _checkState()
+    {
+        if (!$this->getId()) {
+            return $this;
+        }
+
+        $userNotification = $this->hasCustomerNoteNotify() ? $this->getCustomerNoteNotify() : null;
+
+        if (!$this->isCanceled()
+            && !$this->canUnhold()
+            && !$this->canInvoice()
+            && !$this->canShip()) {
+            if (0 == $this->getBaseGrandTotal() || $this->canCreditmemo()) {
+                if ($this->getState() !== self::STATE_COMPLETE && $this->getState() !== 'updated') {
+                    $this->_setState(self::STATE_COMPLETE, true, '', $userNotification);
+                }
+            }
+            /**
+             * Order can be closed just in case when we have refunded amount.
+             * In case of "0" grand total order checking ForcedCanCreditmemo flag
+             */
+            elseif (floatval($this->getTotalRefunded()) || (!$this->getTotalRefunded()
+                && $this->hasForcedCanCreditmemo())
+            ) {
+                if ($this->getState() !== self::STATE_CLOSED) {
+                    $this->_setState(self::STATE_CLOSED, true, '', $userNotification);
+                }
+            }
+        }
+
+        if ($this->getState() == self::STATE_NEW && $this->getIsInProcess()) {
+            $this->setState(self::STATE_PROCESSING, true, '', $userNotification);
+        }
+        return $this;
+    }
+
 }
