7b5b79920bc11ebb2284ef052a6cb34cc6b0d28e
jonathan widro <jwidro@web-jonathan.totsy.net>
2012-08-08 15:55:49 +0000

    changes for inventory still in progress

--- app/code/local/Harapartners/Fulfillmentfactory/Model/Service/Dotcom.php
+++ app/code/local/Harapartners/Fulfillmentfactory/Model/Service/Dotcom.php
@@ -214,6 +214,24 @@ XML;
                 XMLReader::END_ELEMENT == $reader->nodeType
             ) {
                 $state = 'waiting';
+
+                //update product attribute 
+		        //$product = $this->getProductModel();
+	            //$productId = $product->getIdBySku((string)$item->sku);		        
+	            //$product->load($productId);
+                
+                
+                //$product = Mage::getModel('catalog/product')->loadBySku($sku);
+		        $product = Mage::getModel('catalog/product');
+		        
+	            $productId = $product->getIdBySku((string)$item->sku);		        
+	            $product->load($productId);
+	            
+	            if($product&&$product->getId()){
+	                $product->setData('fulfillment_inventory', $qty);                
+	                $product->getResource()->saveAttribute($product, 'fulfillment_inventory');
+				}                
+
                 /* @todo update a local data store for product $sku to have quantity $qty
                 $product = Mage::getModel('catalog/product')->loadBySku($sku);
                 $product->setFulfillmentInventory($qty);
