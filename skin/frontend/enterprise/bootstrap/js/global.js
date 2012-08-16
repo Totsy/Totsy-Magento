d78456a867785f21da8bdfbaef5cff4fe8c75d73
David Gowrie <david.gowrie@gmail.com>
2012-08-16 22:14:34 +0000

    MGN-926 Fixing nav menu dropdown display issues for iPad... Also adding the global.js file

--- skin/frontend/enterprise/bootstrap/js/global.js
+++ skin/frontend/enterprise/bootstrap/js/global.js
@@ -1,7 +1,6 @@
 // All jQuery that is global for the site
-// wrapped in self-executing anonymous function for no conflict use of $ alias
-// appropriately name-spaced
-// not implemented, maybe a phase 2 cleanup
+// - wrapped in self-executing anonymous function for no conflict use of $ alias
+// - use appropriate namespaceing (not implemented, maybe a phase 2 cleanup)
 (function($) {
 
     // TopCartContent namespace
@@ -15,6 +14,23 @@
     // DOM is ready
     $(function(){
         // now do stuff
+ 
+ 
+        /* Prevent default if menu links are "#" */
+        $('nav a').each( function() {
+        	var nav = $(this); 
+        	if( nav.length > 0 ) {
+        		if( nav.attr('href') == '#' ) {
+        			//console.log(nav);
+        			$(this).click(
+        				function(e) {
+        					e.preventDefault();
+        				}
+        			);
+        		}
+        	}
+        }); 
+ 
     
     });
     
