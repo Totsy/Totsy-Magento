79e7a4fe4707f393480d76e0afe25a04b2080fc5
David Gowrie <david.gowrie@gmail.com>
2012-08-17 04:51:33 +0000

    Adding namespace examples, for my own benefit (Evan will likely rock this for realz)

--- skin/frontend/enterprise/bootstrap/js/global.js
+++ skin/frontend/enterprise/bootstrap/js/global.js
@@ -1,19 +1,38 @@
-// All jQuery that is global for the site
+// Sandbox for all jQuery that is global for the site
 // - wrapped in self-executing anonymous function for no conflict use of $ alias
-// - use appropriate namespaceing (not implemented, maybe a phase 2 cleanup)
+// - use appropriate namespacing, don't pollute global namespace (not implemented, maybe a phase 2 cleanup)
 (function($) {
 
-    // TopCartContent namespace
-    if (typeof(TopCartContent) === "undefined") {
-        TopCartContent = {
-            // stuff here
+    // SillyExample namespace
+    if (typeof(SillyExample) === "undefined") {
+        var SillyExample = {
+            init : function() {
+                // define events handlers and such
+                $('#logo').on('click', SillyExample.clickHandler);
+            },
+            
+            // results / actions
+            clickHandler : function() {
+                alert('OOPs there goes the namespace');
+            }
         };
     }
+    
+    // MyNameSpace
+    if (typeof(MyNameSpace) === 'undefined') {
+        var MyNameSpace = {
+            init: function() {
+                console.log('Running MyNameSpace.init');
+            }
+        };
+    }
+    
 
-
-    // DOM is ready
-    $(function(){
-        // now do stuff
+    // DOM is ready, now do stuf
+    $(function() {
+        
+        //SillyExample.init();
+        //MyNameSpace.init();
  
  
         /* Prevent default if menu links are "#" */
