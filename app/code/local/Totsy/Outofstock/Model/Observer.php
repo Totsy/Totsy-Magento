abe25d27cd8908965648fa8fd7f7d52b9bf143c9
Jrek <slv.kosh@gmail.com>
2012-08-27 18:15:42 +0000

    rm crappy code

--- app/code/local/Totsy/Outofstock/Model/Observer.php
+++ app/code/local/Totsy/Outofstock/Model/Observer.php
@@ -9,10 +9,6 @@
 class Totsy_Outofstock_Model_Observer {
 	public function catalogProductUpdate ($observer){	
 		$items = $observer->getEvent()->getOrder()->getAllItems();   
-        
-        $fh = fopen('/tmp/skItems.log','a');
-        fwrite($fh, is_object($items)?'obejct..'.get_class($items):'something else'."\n\n");
-        fclose($fh);
 
         foreach ($items as $item){
             Mage::getModel('product/outofstock')->adminAfterSaveUpdateQty($item);
