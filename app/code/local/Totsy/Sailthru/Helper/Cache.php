16eae28cba10690f481eaae6a994ac2735e60c6e
Jrek <slv.kosh@gmail.com>
2012-08-31 14:44:58 +0000

    move closer to the coding standarts

--- app/code/local/Totsy/Sailthru/Helper/Cache.php
+++ app/code/local/Totsy/Sailthru/Helper/Cache.php
@@ -1,195 +1,230 @@
 <?php
-
 /**
- * @category    Totsy
- * @package     Totsy_Sailthru
- * @author      Slavik Koshelevskyy <skosh@totsy.com>
- * @copyright   Copyright (c) 2012 Totsy LLC
+ * PHP Version 5.3
+ *
+ * @category  Totsy
+ * @package   Totsy_Sailthru
+ * @author    Slavik Koshelevskyy <skosh@totsy.com>
+ * @copyright 2012 Totsy LLC Copyright (c) 
  */
+class Totsy_Sailthru_Helper_Cache
+{
 
-class Totsy_Sailthru_Helper_Cache{
+    // set cache time for 3 days 
+    // 3*24*60*60
+    const TTL = 259200; 
 
-	// set cache time for 3 days 
-	// 3*24*60*60
-	const TTL = 259200; 
+    private $_full_path = null;
+    private $_originHttpHost = 'totsy.com';
+    private $_isHttpHostChanged = false;
+    private $_cache_dir_path = '/var/tmp/';
+    private $_cache_file = null;
+    private $_cache_file_ext = '.json';
+    private $_cache_skip_check = false;
 
-	private $_full_path = null;
-	private $_originHttpHost = 'totsy.com';
-	private $_isHttpHostChanged = false;
-	private $_cache_dir_path = '/var/tmp/';
-	private $_cache_file = null;
-	private $_cache_file_ext = '.json';
-	private $_cache_skip_check = false;
+    /**
+    * This method does all diry caching job.
+    * On succes returs json encoded string or booelan false!
+    *
+    * @param string $full_path full path to caching directory
+    *
+    * @return   string/boolean  
+    */
+    public function runner($full_path = null)
+    {
+        
+        if (!empty($full_path)) {
+            $this->_full_path = $full_path;
+        }
 
-	/**
-	* This method does all diry caching job.
-	* On succes returs json encoded string or booelan false!
-	*
-	* @param    string $full_path full path to caching directory
-	* @return   string/boolean  
-	*/
- 	public function runner($full_path = null){
- 		if (!empty($full_path)){
- 			$this->_full_path = $full_path;
- 		}
- 		if (empty($this->_full_path)){
- 			throw new Exception ('Unknown cache location.');
- 		}
- 		if (!empty($_SERVER['HTTP_HOST'])){
- 			$this->_originHttpHost = $_SERVER['HTTP_HOST'];
- 		}
- 		// handle cli params first
- 		$this->_doCliParams();
- 		// make sure all cache related params are taking its place
- 		$this->_handleGetParams();
- 		// how we have domain and get params, so generate a cache filename
- 		$this->_generateCacheFileName();
- 		// touch cache file
- 		$this->_touchNewCacheFile();
- 		// decide wheather to use cached data or not
- 		$cached = $this->cacheDecision();
- 		// rewrite doamin name, if needed
- 		$this->_setRightHttpHost($cached);
+        if (empty($this->_full_path)) {
+            throw new Exception('Unknown cache location.');
+        }
+        
+        if (!empty($_SERVER['HTTP_HOST'])) {
+            $this->_originHttpHost = $_SERVER['HTTP_HOST'];
+        }
 
- 		return $cached;
- 	}
+        // handle cli params first
+        $this->_doCliParams();
+        // make sure all cache related params are taking its place
+        $this->_handleGetParams();
+        // how we have domain and get params, so generate a cache filename
+        $this->_generateCacheFileName();
+        // touch cache file
+        $this->_touchNewCacheFile();
+        // decide wheather to use cached data or not
+        $cached = $this->cacheDecision();
+        // rewrite doamin name, if needed
+        $this->_setRightHttpHost($cached);
 
- 	/**
- 	* Save processed data into cache file
- 	*
- 	* @param string* $data json encoded sting
- 	* @return void
- 	*/
- 	public function rememberCache(&$data){
- 		
- 		// rewrite doamin name, if needed
- 		$this->_setRightHttpHost($data);
- 		
- 		// write all data into cache file
-		$fh = fopen($this->_getFullCacheFileName(),'w');
-		fwrite($fh,$data);
-		fclose($fh);
+        return $cached;
+    }
 
-		// remove unused cache file
-		if (file_exists($this->_getFullCacheFileName().'.new')){
-			unlink($this->_getFullCacheFileName().'.new');
-		}
-	}
+    /**
+    * Save processed data into cache file
+    *
+    * @param string &$data json encoded sting
+    *
+    * @return void
+    */
+    public function rememberCache(&$data)
+    {
+        
+        // rewrite doamin name, if needed
+        $this->_setRightHttpHost($data);
+        
+        // write all data into cache file
+        $fh = fopen($this->_getFullCacheFileName(), 'w');
+        fwrite($fh, $data);
+        fclose($fh);
 
- 	/**
- 	* Makes a decision weather get data from cached file or not
- 	*
- 	* @param void
- 	* @return string/boolean 
- 	*/
- 	public function cacheDecision(){
+        // remove unused cache file
+        if (file_exists($this->_getFullCacheFileName().'.new')) {
+            unlink($this->_getFullCacheFileName().'.new');
+        }
 
- 		if (file_exists($this->_getFullCacheFileName()) && $this->_cache_skip_check===false){
-			$time = time() - filectime( $this->_getFullCacheFileName() );
-			if ($time<self::$TTL || file_exists( $this->_getFullCacheFileName().'.new')){
-				return file_get_contents( $this->_getFullCacheFileName() );
-			}
-		}
+    }
 
-		return false;
- 	}
+    /**
+    * Makes a decision weather get data from cached file or not
+    *
+    * @return string/boolean 
+    */
+    public function cacheDecision()
+    {
 
- 	/**
- 	* make sure to get all necessary data even from get params
- 	*
- 	* @param void
- 	* @return void
- 	*/
- 	private function _handleGetParams(){
- 		if (!empty($_GET['domain']) && stripos($_GET['domain'],'mamasource.totsy.com')){
-			$this->_setHttHost('mamasource.totsy.com');
-		}
- 	}
+        if (file_exists($this->_getFullCacheFileName()) 
+            && $this->_cache_skip_check===false
+        ) {
 
- 	/**
- 	* Prepare new cache file for current request
- 	*
- 	* @param void
- 	* @return void
- 	*/
- 	private function _touchNewCacheFile(){
- 		if ($this->_cache_skip_check===true){
-			file_put_contents(
-				$this->_getFullCacheFileName().'.new',
-				'addin some cache'
-			);
-		}
- 	}
+            $time = time() - filectime($this->_getFullCacheFileName());
+            
+            if ($time<self::$TTL 
+                || file_exists($this->_getFullCacheFileName().'.new')
+            ) {
+                return file_get_contents($this->_getFullCacheFileName());
+            }
 
- 	/**
- 	* Generate a new cache file name
- 	*
- 	* @param void
- 	* @return void
- 	*/
- 	private function _generateCacheFileName(){
- 		$this->_cache_file = md5($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']).$this->_cache_file_ext;
- 	}
+        }
 
- 	/**
- 	* Dispatch cli params
- 	*
- 	* @param void
- 	* @return void
- 	*/
- 	private function _doCliParams(){
+        return false;
+    }
 
- 		if (php_sapi_name()=='cli'){
-			$this->_cache_skip_check = true;
+    /**
+    * make sure to get all necessary data even from get params
+    *
+    * @return void
+    */
+    private function _handleGetParams()
+    {
+    
+        if (!empty($_GET['domain']) 
+            && stripos($_GET['domain'], 'mamasource.totsy.com')
+        ) {
+            $this->_setHttHost('mamasource.totsy.com');
+        }
+    
+    }
 
-			$index=array_search('--get', $argv);
-			if ( $index!==false && $argc>$index+1) {
-				 $_SERVER['REQUEST_URI'] = $argv[$index+1];
-			}
+    /**
+    * Prepare new cache file for current request
+    *
+    * @return void
+    */
+    private function _touchNewCacheFile()
+    {
+    
+        if ($this->_cache_skip_check===true) {
+            file_put_contents(
+                $this->_getFullCacheFileName().'.new',
+                'addin some cache'
+            );
+        }
 
-			$index=array_search('--domain', $argv);
-			if ( $index!==false && $argc>$index+1) {
-				$this->_setHttHost($argv[$index+1]);
-			}
-		}
+    }
 
- 	}
+    /**
+    * Generate a new cache file name
+    *
+    * @return void
+    */
+    private function _generateCacheFileName()
+    {
+        $this->_cache_file = md5(
+            $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']
+        ).$this->_cache_file_ext;
+    }
 
- 	/**
- 	* Repalces SERVER HTTP_HOST for current session only
- 	*
- 	* @param string $httHost new HTTP_HOST value
- 	* @return void
- 	*/
- 	private function _setHttHost($httHost){
- 		$_SERVER['HTTP_HOST'] = $httHost;
- 		$isHttpHostChanged = true;
- 	}
+    /**
+    * Dispatch cli params
+    *
+    * @return void
+    */
+    private function _doCliParams()
+    {
 
- 	/**
- 	* Build full path to cache file
-	*
- 	* @param void
- 	* @return string 
- 	*/
- 	private function _getFullCacheFileName(){
-		return  $file = $this->_full_path.
-				$this->_cache_dir_path.
-				$this->_cache_file;
- 	}
+        if (php_sapi_name()=='cli') {
+            $this->_cache_skip_check = true;
 
- 	/**
- 	* In Case we need to override current HTTP_HOST (domain) value,
- 	* we doing it here. 
- 	*
- 	* @param string* $json referenced string to json encoded string  
- 	* @return void
- 	*/
-	private function _setRightHttpHost(&$json){
-		if ( $this->_isHttpHostChanged==true ){
-			$json = str_replace($this->_originHttpHost, $_SERVER['HTTP_HOST'], $json);
-		}
-	}
+            $index=array_search('--get', $argv);
+            if ( $index!==false && $argc>$index+1) {
+                 $_SERVER['REQUEST_URI'] = $argv[$index+1];
+            }
+
+            $index=array_search('--domain', $argv);
+            if ( $index!==false && $argc>$index+1) {
+                $this->_setHttHost($argv[$index+1]);
+            }
+        }
+
+    }
+
+    /**
+    * Repalces SERVER HTTP_HOST for current session only
+    *
+    * @param string $httHost new HTTP_HOST value
+    *
+    * @return void
+    */
+    private function _setHttHost($httHost) 
+    {
+        $_SERVER['HTTP_HOST'] = $httHost;
+        $isHttpHostChanged = true;
+    }
+
+    /**
+    * Build full path to cache file
+    *
+    * @return string 
+    */
+    private function _getFullCacheFileName() 
+    {
+        return  $file = $this->_full_path.
+                $this->_cache_dir_path.
+                $this->_cache_file;
+    }
+
+    /**
+    * In Case we need to override current HTTP_HOST (domain) value,
+    * we doing it here. 
+    *
+    * @param string &$json referenced string to json encoded string  
+    *
+    * @return void
+    */
+    private function _setRightHttpHost(&$json)
+    {
+
+        if ( $this->_isHttpHostChanged==true ) {
+            $json = str_replace(
+                $this->_originHttpHost, 
+                $_SERVER['HTTP_HOST'], 
+                $json
+            );
+        }
+
+    }
 }
 
 ?>
--- app/code/local/Totsy/Sailthru/Helper/Feed.php
+++ app/code/local/Totsy/Sailthru/Helper/Feed.php
@@ -7,16 +7,90 @@
  * @copyright   Copyright (c) 2012 Totsy LLC
  */
 
-class Totsy_Sailthru_Helper_Feed {
+class Totsy_Sailthru_Helper_Feed
+{
 
-	/**
-	* send NO CACHE json headers 
-	*/
-	public function sendHeaders() {
-		header('Cache-Control: no-cache, must-revalidate');
-		header('Content-type: application/json');
-	}
+    private $_timeDiff = 0;
+    // is default magento time is ahead of actual server time
+    private $_timeIsAhead = false;
+    private $_startDate = null;
+    private $_startTime = null;
+    private $_order = false; // true = DESC; false = ACS
 
+    /**
+    * send NO CACHE json headers 
+    */
+    public function sendHeaders()
+    {
+        header('Cache-Control: no-cache, must-revalidate');
+        header('Content-type: application/json');
+    }
+
+    public function setMagentoTimeDiff()
+    {
+        $defaultTimezone = date_default_timezone_get();
+        $mageTimezone = Mage::getStoreConfig(
+            Mage_Core_Model_Locale::XML_PATH_DEFAULT_TIMEZONE
+        );
+        date_default_timezone_set($mageTimezone);
+        $time = time();
+        date_default_timezone_set($defaultTimezone);
+        $defaultTime = time();
+
+        if ($defaultTime>$time) {
+            $this->_timeDiff = $defaultTime - $time;
+            $this->_timeIsAhead = false
+        } else {
+            $this->_timeDiff = $time - $defaultTime;
+            $this->_timeIsAhead = true;
+        }
+
+    } 
+
+    private function _processStartDate()
+    {
+        $this->_startDate = strtotime(date('Y-m-d'));
+
+        if (empty($_GET['start_date'])) { 
+            return;
+        }
+
+        if ( preg_match('/[\d]{4}[\-][\d]{2}[\-][\d]{2}/i', $_GET['start_date'], $m)) {
+            $this->_startDate = strtotime($m[0]);
+        }
+    }
+
+    private function _processStartTime()
+    {
+        if (!empty($_GET['start_time']) 
+            && preg_match('/[\w]{2}/', $_GET['start_time']) 
+            && strtolower($_GET['start_time']) == 'am' 
+        ) {
+            $this->_startTime = '08:00:00';
+        }
+
+        if (!empty($_GET['start_time'])
+            && preg_match('/[\d]{2}[\:][\d]{2}[\:][\d]{2}/', $_GET['start_time'])
+        ) {
+            $st = preg_replace('/[^\d\:]+/', '', $_GET['start_time']);
+            if (strlen($st)==8) {
+                $this->_startTime = $st;
+                unset($st);
+            }
+        }
+
+    }
+
+    private function _processOrder ()
+    {
+        
+        if (empty($_GET['order'])) {
+            return;
+        }
+
+        if (strtolower($_GET['order']) == 'desc') {
+            $this->_order = true; //DESC
+        }
+
+    }
 }
-
-?>
