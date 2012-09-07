ee512ca10a1b1ead6acaa1dca5df8a75e4b97c49
Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
2012-09-07 16:41:16 +0000

    Added new ZipCodeInfo model to the Customer module, with a setup script to create a table of zip code information.

---
+++ app/code/local/Totsy/Customer/sql/totsycustomer_setup/mysql4-upgrade-0.1.0-0.2.0.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @category    Totsy
+ * @package     Totsy
+ * @author      Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
+ * @copyright   Copyright (c) 2012 Totsy LLC
+ */
+
+$installer = $this;
+$installer->startSetup();
+
+$installer->run("
+    -- DROP TABLE IF EXISTS {$this->getTable('totsycustomer/zipCodeInfo')};
+    CREATE TABLE {$this->getTable('totsycustomer/zipCodeInfo')} (
+        `customer_zip_code_info_id` int(10) unsigned NOT NULL auto_increment,
+        `zip` char(5) DEFAULT NULL COMMENT 'ZIP Code',
+        `city` varchar(255) DEFAULT NULL COMMENT 'City',
+        `state` char(2) DEFAULT NULL COMMENT 'State',
+        PRIMARY KEY (`customer_zip_code_info_id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='ZIP Code Information';
+");
+
+$installer->endSetup();
