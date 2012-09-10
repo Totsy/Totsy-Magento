1f4d3d1d561cb6555dadcc0efcab7653d52942eb
Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
2012-09-10 17:15:20 +0000

    Added Totsy_Akamai module with interface to the Akamai CCU for issuing content purge requests.

---
+++ app/code/local/Totsy/Akamai/Helper/Service/Ccu.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @category    Totsy
+ * @package     Totsy_Akamai_Helper
+ * @author      Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
+ * @copyright   Copyright (c) 2012 Totsy LLC
+ */
+
+class Totsy_Akamai_Helper_Service_Ccu
+{
+    /**
+     * The SOAP client.
+     *
+     * @var \SoapClient
+     */
+    protected $_client;
+
+    /**
+     * The Akamai CCU username.
+     *
+     * @var string
+     */
+    protected $_username;
+
+    /**
+     * The Akamai CCU password.
+     *
+     * @var string
+     */
+    protected $_password;
+
+    public function __construct($username = null, $password = null)
+    {
+        $this->_client = new SoapClient("https://ccuapi.akamai.com/ccuapi-axis.wsdl");
+        $this->_username = $username ?: Mage::getStoreConfig('web/akamai/username');
+        $this->_password = $password ?: Mage::getStoreConfig('web/akamai/password');
+    }
+
+    /**
+     * Issue a purge request to the Akamai Content Control Utility.
+     *
+     * @param array|string $url
+     *
+     * @return int
+     */
+    public function purge($url)
+    {
+        $url = (array) $url;
+        $this->_client->purgeRequest(
+            $this->_username, // name
+            $this->_password, // pwd
+            '',               // network
+            array(),          // opt
+            $url              // uri
+        );
+
+        return 0;
+    }
+
+    /**
+     * @param \SoapClient $client
+     */
+    public function setClient($client)
+    {
+        $this->_client = $client;
+    }
+
+    /**
+     * @return \SoapClient
+     */
+    public function getClient()
+    {
+        return $this->_client;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->_password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->_password;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->_username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->_username;
+    }
+}
