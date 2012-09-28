34c3b1da3f0d406dc200de7480fd5db79d936f89
troyer <troyer@totsy.com>
2012-09-28 21:37:43 +0000

    If an address is linked with a credit card, block any deletion or edit

---
+++ app/code/local/Totsy/Customer/Block/Address/Book.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @category    Totsy
+ * @package     Totsy_Customer_Block_Address_Book
+ * @author      Tom Royer <troyer@totsy.com>
+ * @copyright   Copyright (c) 2012 Totsy LLC
+ */
+class Totsy_Customer_Block_Address_Book extends Mage_Customer_Block_Address_Book
+{
+    public function isAddressLinkWithPaymentProfile($addressId)
+    {
+        $linked = false;
+        if($addressId) {
+            $profile = Mage::getModel('paymentfactory/profile')->load($addressId, 'address_id');
+            if($profile->getId()) {
+                $linked = true;
+            }
+        }
+        return $linked;
+    }
+}
