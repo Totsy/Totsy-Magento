12cfa9ba2b79a9f55bbb0205afcf964a091aed8f
Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
2012-10-04 16:09:26 +0000

    Added 'Internal Department' product attribute.

---
+++ app/code/local/Totsy/Catalog/sql/totsycatalog_setup/mysql4-upgrade-0.1.2-0.1.3.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @category    Totsy
+ * @package     Totsy
+ * @author      Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
+ * @copyright   Copyright (c) 2012 Totsy LLC
+ */
+
+$installer = $this;
+
+$installer->installEntities();
