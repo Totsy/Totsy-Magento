e0dbb79802659832a67d1e2c02e695c4e58da3aa
Evan Ubiera <evanubiera@gmail.com>
2012-10-09 20:51:34 +0000

    Revert "Merge branch 'hack/address-generation' of github.com:Totsy/Totsy-Magento into feature/socialablelabs"
    This reverts commit 64eec8cbbd3f83c0cdc4125f73655337acaa9b18, reversing
    changes made to ef8ccadcde45a025a196a56a91a771e3052cb817.
    
    Conflicts:
    
    	app/code/local/Totsy/Customer/sql/totsycustomer_setup/mysql4-upgrade-0.1.0-0.2.0.php

--- app/code/local/Totsy/Customer/sql/totsycustomer_setup/mysql4-upgrade-0.1.0-0.2.0.php
+++ app/code/local/Totsy/Customer/sql/totsycustomer_setup/mysql4-upgrade-0.1.0-0.2.0.php
@@ -1,33 +1,8 @@
 <?php
-<<<<<<< HEAD
-/**
- * @category    Totsy
- * @package     Totsy
- * @author      Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
- * @copyright   Copyright (c) 2012 Totsy LLC
- */
-
-=======
->>>>>>> 2d51cfbf841ee59c60830d650424fd08c75bd248
 $installer = $this;
 $installer->startSetup();
 
 $installer->run("
-<<<<<<< HEAD
-    -- DROP TABLE IF EXISTS {$this->getTable('totsycustomer/zipCodeInfo')};
-    CREATE TABLE {$this->getTable('totsycustomer/zipCodeInfo')} (
-        `customer_zip_code_info_id` int(10) unsigned NOT NULL auto_increment,
-        `zip` char(5) DEFAULT NULL COMMENT 'ZIP Code',
-        `city` varchar(255) DEFAULT NULL COMMENT 'City',
-        `state` char(2) DEFAULT NULL COMMENT 'State',
-        `type` varchar(32) DEFAULT 'STANDARD' COMMENT 'Zip Code Type',
-        PRIMARY KEY (`customer_zip_code_info_id`),
-        INDEX (`zip`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='ZIP Code Information';
-");
-
-$installer->endSetup();
-=======
     SET FOREIGN_KEY_CHECKS = 0;
     ALTER TABLE paymentfactory_profile
     ADD FOREIGN KEY (`address_id`)
@@ -36,4 +11,3 @@ $installer->endSetup();
 ");
 
 $installer->endSetup();
->>>>>>> 2d51cfbf841ee59c60830d650424fd08c75bd248
