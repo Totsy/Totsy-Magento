200729c1db6d1896b9003740d3c507d2e26c3fad
Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
2012-10-11 21:32:39 +0000

    Moved Checkout module rewrites from Harapartners_Service into Totsy_Checkout module, and cleaned up code in the process.

--- app/code/local/Harapartners/Service/Helper/Data.php
+++ app/code/local/Harapartners/Service/Helper/Data.php
@@ -2,4 +2,8 @@
 
 class Harapartners_Service_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    public function isTotsyStore()
+    {
+        return Mage::app()->getStore()->getCode() == 'default';
+    }
 }
