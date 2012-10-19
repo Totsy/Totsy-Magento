d10699c7702a64cf38d225c96457deb6a76269cf
troyer <troyer@totsy.com>
2012-10-19 22:11:35 +0000

    Remove Discount Code

--- app/code/community/TinyBrick/OrderEdit/Model/Edit/Updater/Type/Eitems.php
+++ app/code/community/TinyBrick/OrderEdit/Model/Edit/Updater/Type/Eitems.php
@@ -24,26 +24,16 @@
  */
 class TinyBrick_OrderEdit_Model_Edit_Updater_Type_Eitems extends TinyBrick_OrderEdit_Model_Edit_Updater_Type_Abstract
 {
-	public function edit(TinyBrick_OrderEdit_Model_Order $order, $data = array())
-	{
-		$comment = "";
+    public function edit(TinyBrick_OrderEdit_Model_Order $order, $data = array())
+    {
+        $comment = "";
         $itemCounter = 0;
         $discountFixed = null;
-        if($order->getDiscountDescription()) {
-            $rule = Mage::getModel('salesrule/rule');
-            $labels = $rule->getStoreLabels();
-            $ruleLabel = Mage::getModel('salesrule/rule_label')->load($order->getDiscountDescription(), 'description');
-            $rule = Mage::getModel('salesrule/rule_label')->load($ruleLabel->getRuleId());
-            if($rule->getSimpleAction == 'cart_fixed') {
-                $discountFixed = $rule->getDiscountAmount();
-            }
-        }
-
-		foreach($data['id'] as $key => $itemId) {
+       foreach($data['id'] as $key => $itemId) {
             $itemCounter++;
-			$item = $order->getItemById($itemId);
-			if($data['remove'][$key]) {
-				$comment .= "Removed Item(SKU): " . $item->getSku() . "<br />";
+            $item = $order->getItemById($itemId);
+            if($data['remove'][$key]) {
+                $comment .= "Removed Item(SKU): " . $item->getSku() . "<br />";
                 //Changing Status of Item Queue
                 if(Mage::getModel('sales/order_item')->load($itemId,'parent_item_id')->getId()) {
                     $itemQueueItemId = Mage::getModel('sales/order_item')->load($itemId,'parent_item_id')->getId();
@@ -59,26 +49,20 @@ class TinyBrick_OrderEdit_Model_Edit_Updater_Type_Eitems extends TinyBrick_Order
                 if($childItemId) {
                     $order->removeItem($childItemId);
                 }
-			} else {
-				$oldArray = array('price'=>$item->getPrice(), 'discount'=>$item->getDiscountAmount(), 'qty'=>$item->getQtyOrdered());
-				$item->setPrice($data['price'][$key]);
-				$item->setBasePrice($data['price'][$key]);
-				$item->setBaseOriginalPrice($data['price'][$key]);
-				$item->setOriginalPrice($data['price'][$key]);
-				$item->setBaseRowTotal($data['price'][$key]);
-                if($discountFixed) {
-                    if($itemCounter == 1) {
-                        $item->setDiscountAmount($discountFixed);
-                    }
-                } else {
-                    if($data['discount'][$key]) {
-                        $item->setDiscountAmount($data['discount'][$key]);
-                    }
+            } else {
+                $oldArray = array('price'=>$item->getPrice(), 'discount'=>$item->getDiscountAmount(), 'qty'=>$item->getQtyOrdered());
+                $item->setPrice($data['price'][$key]);
+                $item->setBasePrice($data['price'][$key]);
+                $item->setBaseOriginalPrice($data['price'][$key]);
+                $item->setOriginalPrice($data['price'][$key]);
+                $item->setBaseRowTotal($data['price'][$key]);
+                if($data['discount'][$key]) {
+                    $item->setDiscountAmount($data['discount'][$key]);
                 }
-				if($data['qty'][$key]) {
-					$item->setQtyOrdered($data['qty'][$key]);
-				}
-				$item->save();
+                if($data['qty'][$key]) {
+                    $item->setQtyOrdered($data['qty'][$key]);
+                }
+                $item->save();
                 if((int)$oldArray['qty'] > (int)$data['qty'][$key]) {
                     $returnedQuantity = ((int)$oldArray['qty'] - (int)$data['qty'][$key]);
                     Mage::getSingleton('cataloginventory/stock')->backItemQty($item->getProductId(),$returnedQuantity);
@@ -86,25 +70,25 @@ class TinyBrick_OrderEdit_Model_Edit_Updater_Type_Eitems extends TinyBrick_Order
                     Mage::getSingleton('cataloginventory/stock')->backItemQty($item->getProductId(),(int)$oldArray['qty']);
                     Mage::getSingleton('cataloginventory/stock')->registerItemSale($item);
                 }
-				$newArray = array('price'=>$item->getPrice(), 'discount'=>$item->getDiscountAmount(), 'qty'=>$item->getQtyOrdered());
-				if($newArray['price'] != $oldArray['price'] || $newArray['discount'] != $oldArray['discount'] || $newArray['qty'] != $oldArray['qty']) {
-					$comment = "Edited item " . $item->getSku() . "<br />";
-					if($newArray['price'] != $oldArray['price']) {
-						$comment .= "Price FROM: " . $oldArray['price'] . " TO: " . $newArray['price'] . "<br />";
-					}
-					if($newArray['discount'] != $oldArray['discount']) {
-						$comment .= "Discount FROM: " . $oldArray['discount'] . " TO: " . $newArray['discount'] . "<br />";
-					}
-					if($newArray['qty'] != $oldArray['qty']) {
-						$comment .= "Qty FROM: " . $oldArray['qty'] . " TO: " . $newArray['qty'] . "<br />";
-					}
-				}
-			}
-		}
-		if($comment != "") {
-			$comment = "Edited items:<br />" . $comment . "<br />";
-			return $comment;
-		}
-		return true;
-	}
+                $newArray = array('price'=>$item->getPrice(), 'discount'=>$item->getDiscountAmount(), 'qty'=>$item->getQtyOrdered());
+                if($newArray['price'] != $oldArray['price'] || $newArray['discount'] != $oldArray['discount'] || $newArray['qty'] != $oldArray['qty']) {
+                    $comment = "Edited item " . $item->getSku() . "<br />";
+                    if($newArray['price'] != $oldArray['price']) {
+                        $comment .= "Price FROM: " . $oldArray['price'] . " TO: " . $newArray['price'] . "<br />";
+                    }
+                    if($newArray['discount'] != $oldArray['discount']) {
+                        $comment .= "Discount FROM: " . $oldArray['discount'] . " TO: " . $newArray['discount'] . "<br />";
+                    }
+                    if($newArray['qty'] != $oldArray['qty']) {
+                        $comment .= "Qty FROM: " . $oldArray['qty'] . " TO: " . $newArray['qty'] . "<br />";
+                    }
+                }
+            }
+        }
+        if($comment != "") {
+            $comment = "Edited items:<br />" . $comment . "<br />";
+            return $comment;
+        }
+        return true;
+    }
 }
