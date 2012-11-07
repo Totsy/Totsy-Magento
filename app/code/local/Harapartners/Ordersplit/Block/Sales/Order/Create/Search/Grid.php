47e9d570be7fbe1f072ac0726d4544f85aedf379
Ryan Street <rstreet@crownpartners.com>
2012-11-07 15:21:19 -0500

    Adminhtml fulfillment type logic

---
+++ app/code/local/Harapartners/Ordersplit/Block/Sales/Order/Create/Search/Grid.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ryan.street
+ * Date: 11/7/12
+ * Time: 2:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Harapartners_Ordersplit_Block_Sales_Order_Create_Search_Grid extends Mage_Adminhtml_Block_Sales_Order_Create_Search_Grid {
+    /**
+     * Prepare columns
+     *
+     * @return Mage_Adminhtml_Block_Sales_Order_Create_Search_Grid
+     */
+    protected function _prepareColumns()
+    {
+        $this->addColumn('entity_id', array(
+            'header'    => Mage::helper('sales')->__('ID'),
+            'sortable'  => true,
+            'width'     => '60',
+            'index'     => 'entity_id'
+        ));
+        $this->addColumn('name', array(
+            'header'    => Mage::helper('sales')->__('Product Name'),
+            'renderer'  => 'adminhtml/sales_order_create_search_grid_renderer_product',
+            'index'     => 'name'
+        ));
+        $this->addColumn('sku', array(
+            'header'    => Mage::helper('sales')->__('SKU'),
+            'width'     => '80',
+            'index'     => 'sku'
+        ));
+
+        $this->addColumn('fulfillment', array(
+                'header' => Mage::helper('sales')->__('Fulfillment Type'),
+                'renderer' => 'adminhtml/sales_order_create_search_grid_renderer_fulfillment',
+                'index' => 'fulfillment',
+                'width' => '80'
+            )
+        );
+
+        $this->addColumn('price', array(
+            'header'    => Mage::helper('sales')->__('Price'),
+            'column_css_class' => 'price',
+            'align'     => 'center',
+            'type'      => 'currency',
+            'currency_code' => $this->getStore()->getCurrentCurrencyCode(),
+            'rate'      => $this->getStore()->getBaseCurrency()->getRate($this->getStore()->getCurrentCurrencyCode()),
+            'index'     => 'price',
+            'renderer'  => 'adminhtml/sales_order_create_search_grid_renderer_price',
+        ));
+
+        $this->addColumn('in_products', array(
+            'header'    => Mage::helper('sales')->__('Select'),
+            'header_css_class' => 'a-center',
+            'type'      => 'checkbox',
+            'name'      => 'in_products',
+            'values'    => $this->_getSelectedProducts(),
+            'align'     => 'center',
+            'index'     => 'entity_id',
+            'sortable'  => false,
+        ));
+
+        $this->addColumn('qty', array(
+            'filter'    => false,
+            'sortable'  => false,
+            'header'    => Mage::helper('sales')->__('Qty To Add'),
+            'renderer'  => 'adminhtml/sales_order_create_search_grid_renderer_qty',
+            'name'      => 'qty',
+            'inline_css'=> 'qty',
+            'align'     => 'center',
+            'type'      => 'input',
+            'validate_class' => 'validate-number',
+            'index'     => 'qty',
+            'width'     => '1',
+        ));
+
+        return parent::_prepareColumns();
+    }
+}
