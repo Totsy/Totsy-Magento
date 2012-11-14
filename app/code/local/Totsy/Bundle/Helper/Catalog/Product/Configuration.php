83a8f4f414e1242eb09b79b87615534110b643ba
Chris Davidowski <cdavidowski@crownpartners.com>
2012-11-14 05:59:51 +0000

    added display of bundled products on product view, cart page, checkout page, and customer order view

---
+++ app/code/local/Totsy/Bundle/Helper/Catalog/Product/Configuration.php
@@ -0,0 +1,70 @@
+<?php
+class Totsy_Bundle_Helper_Catalog_Product_Configuration extends Mage_Bundle_Helper_Catalog_Product_Configuration
+{
+    /**
+     * Get bundled selections (slections-products collection)
+     *
+     * Returns array of options objects.
+     * Each option object will contain array of selections objects
+     *
+     * @return array
+     */
+    public function getBundleOptions(Mage_Catalog_Model_Product_Configuration_Item_Interface $item)
+    {
+        $options = array();
+        $product = $item->getProduct();
+
+        /**
+         * @var Mage_Bundle_Model_Product_Type
+         */
+        $typeInstance = $product->getTypeInstance(true);
+
+        // get bundle options
+        $optionsQuoteItemOption = $item->getOptionByCode('bundle_option_ids');
+        $bundleOptionsIds = unserialize($optionsQuoteItemOption->getValue());
+        if ($bundleOptionsIds) {
+            /**
+            * @var Mage_Bundle_Model_Mysql4_Option_Collection
+            */
+            $optionsCollection = $typeInstance->getOptionsByIds($bundleOptionsIds, $product);
+
+            // get and add bundle selections collection
+            $selectionsQuoteItemOption = $item->getOptionByCode('bundle_selection_ids');
+
+            $selectionsCollection = $typeInstance->getSelectionsByIds(
+                unserialize($selectionsQuoteItemOption->getValue()),
+                $product
+            );
+
+            $bundleOptions = $optionsCollection->appendSelections($selectionsCollection, true);
+            foreach ($bundleOptions as $bundleOption) {
+                if ($bundleOption->getSelections()) {
+                    $option = array(
+                        'label' => $bundleOption->getTitle(),
+                        'value' => array()
+                    );
+
+                    $bundleSelections = $bundleOption->getSelections();
+
+                    foreach ($bundleSelections as $bundleSelection) {
+                        $qty = $this->getSelectionQty($product, $bundleSelection->getSelectionId()) * 1;
+                        if ($qty) {
+                            //2012-11-14 - CJD - Hiding option price if equal to zero per Rob's request
+                            $text = $qty . ' x ' . $this->escapeHtml($bundleSelection->getName());
+                            if($this->getSelectionFinalPrice($item, $bundleSelection) > 0) {
+                                $text .= ' ' . Mage::helper('core')->currency($this->getSelectionFinalPrice($item, $bundleSelection));
+                            }
+                            $option['value'][] = $text;
+                        }
+                    }
+
+                    if ($option['value']) {
+                        $options[] = $option;
+                    }
+                }
+            }
+        }
+
+        return $options;
+    }
+}
