1a76abcd344c88dba2a3a3471ca78a1a5f7dccd3
Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
2013-01-14 21:49:26 +0000

    Cleaned up redundant install/upgrade scripts that were breaking unit test setup.

--- app/code/local/Totsy/Bundle/sql/totsybundle_setup/mysql4-install-0.0.1.php
+++ app/code/local/Totsy/Bundle/sql/totsybundle_setup/mysql4-install-0.0.1.php
@@ -5,19 +5,7 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->run("
-TRUNCATE `catalog_product_link_attribute`;
-INSERT INTO `catalog_product_link_attribute` VALUES ('1', '1', 'position', 'int');
-INSERT INTO `catalog_product_link_attribute` VALUES ('2', '3', 'position', 'int');
-INSERT INTO `catalog_product_link_attribute` VALUES ('3', '3', 'qty', 'decimal');
-INSERT INTO `catalog_product_link_attribute` VALUES ('4', '4', 'position', 'int');
-INSERT INTO `catalog_product_link_attribute` VALUES ('5', '5', 'position', 'int');
-
-TRUNCATE `catalog_product_link_type`;
-INSERT INTO `catalog_product_link_type` VALUES ('1', 'relation');
 INSERT INTO `catalog_product_link_type` VALUES ('2', 'bundle');
-INSERT INTO `catalog_product_link_type` VALUES ('3', 'super');
-INSERT INTO `catalog_product_link_type` VALUES ('4', 'up_sell');
-INSERT INTO `catalog_product_link_type` VALUES ('5', 'cross_sell');
 ");
 
 $installer->endSetup();
