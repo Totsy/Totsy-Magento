352407d82c051ba2303cb5afc8470a5aaa8d9f3e
Chris Davidowski <cdavidowski@crownpartners.com>
2013-01-16 15:56:56 +0000

    added missing files for stock status fix

---
+++ app/code/local/Totsy/CatalogInventory/Model/Stock/Item.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ryan.street
+ * Date: 12/14/12
+ * Time: 2:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Totsy_CatalogInventory_Model_Stock_Item extends Mage_CatalogInventory_Model_Stock_Item {
+
+    /**
+     * Check quantity requested against items in quotes
+     *
+     * @param decimal $qty
+     * @return bool
+     */
+    public function checkQty($qty) {
+        if (!$this->getManageStock() || Mage::app()->getStore()->isAdmin()) {
+            return true;
+        }
+
+        $reserved = Mage::helper('totsy_cataloginventory')->getReserveCount($this->getProductId());
+
+        $calcQty = $this->getQty();
+        if($reserved != false) {
+            Mage::log($reserved.'::'.$calcQty.'::'.$qty);
+            $calcQty = $this->getQty() - $reserved;
+        }
+
+        if ($calcQty - $qty < 0) {
+            switch ($this->getBackorders()) {
+                case Mage_CatalogInventory_Model_Stock::BACKORDERS_YES_NONOTIFY:
+                case Mage_CatalogInventory_Model_Stock::BACKORDERS_YES_NOTIFY:
+                    break;
+                default:
+                    return false;
+                    break;
+            }
+        }
+        return true;
+    }
+}
