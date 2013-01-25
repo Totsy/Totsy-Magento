3f6dbff2ca51011cb6b87d9e83aad93d16aed3b0
Jason Grim <jgrim@crownpartners.com>
2013-01-25 17:42:23 -0500

    Fixed bug with rewards changing observer

--- app/code/local/Crown/Club/Model/Observer.php
+++ app/code/local/Crown/Club/Model/Observer.php
@@ -1,46 +1,46 @@
 <?php
 class Crown_Club_Model_Observer {
 
-	/**
-	 * Moves expired club members back to the non club members group.
-	 * @param Mage_Cron_Model_Schedule $schedule
-	 * @since 0.1.0
-	 * @return void
-	 */
-	public function removeExpiredClubMembers($schedule) {
-		$helper = Mage::helper('crownclub');
-		if (!$helper->moduleSetupComplete()) return;
+    /**
+     * Moves expired club members back to the non club members group.
+     * @param Mage_Cron_Model_Schedule $schedule
+     * @since 0.1.0
+     * @return void
+     */
+    public function removeExpiredClubMembers($schedule) {
+        $helper = Mage::helper('crownclub');
+        if (!$helper->moduleSetupComplete()) return;
 
-		$clubModel = Mage::getModel('crownclub/club');
+        $clubModel = Mage::getModel('crownclub/club');
 
-		$expiredMembers = $clubModel->getExpiredMembersOutOfGracePeriod();
+        $expiredMembers = $clubModel->getExpiredMembersOutOfGracePeriod();
 
-		foreach ($expiredMembers as $expiredMember) {
-			$customer = Mage::getModel('customer/customer')->load($expiredMember->getId());
-			$clubModel->removeClubMember($customer)->sendClubMembershipCancelledEmail($customer);
-		}
-	}
+        foreach ($expiredMembers as $expiredMember) {
+            $customer = Mage::getModel('customer/customer')->load($expiredMember->getId());
+            $clubModel->removeClubMember($customer)->sendClubMembershipCancelledEmail($customer);
+        }
+    }
 
-	/**
-	 * Warns expired club members that their payment has failed and their subscription account
-	 * will be cancelled once it exits the grace period.
-	 * @param Mage_Cron_Model_Schedule $schedule
-	 * @since 0.1.0
-	 * @return void
-	 */
-	public function warnExpiredClubMembers($schedule) {
-		$helper = Mage::helper('crownclub');
-		if (!$helper->moduleSetupComplete()) return;
+    /**
+     * Warns expired club members that their payment has failed and their subscription account
+     * will be cancelled once it exits the grace period.
+     * @param Mage_Cron_Model_Schedule $schedule
+     * @since 0.1.0
+     * @return void
+     */
+    public function warnExpiredClubMembers($schedule) {
+        $helper = Mage::helper('crownclub');
+        if (!$helper->moduleSetupComplete()) return;
 
-		$clubModel = Mage::getModel('crownclub/club');
+        $clubModel = Mage::getModel('crownclub/club');
 
-		$expiredMembers = $clubModel->getExpiredMembersInGracePeriod();
+        $expiredMembers = $clubModel->getExpiredMembersInGracePeriod();
 
-		foreach ($expiredMembers as $expiredMember) {
-			$customer = Mage::getModel('customer/customer')->load($expiredMember->getId());
-			$clubModel->sendClubMembershipPaymentFailedEmail($customer);
-		}
-	}
+        foreach ($expiredMembers as $expiredMember) {
+            $customer = Mage::getModel('customer/customer')->load($expiredMember->getId());
+            $clubModel->sendClubMembershipPaymentFailedEmail($customer);
+        }
+    }
 
     /**
      * Update points balance after order becomes completed
@@ -55,6 +55,14 @@ class Crown_Club_Model_Observer {
         $order = $observer->getEvent()->getOrder();
 
         // Check if order is club membership
+        if ($order->getCustomerIsGuest()
+            || !Mage::helper('enterprise_reward')->isEnabledOnFront($order->getStore()->getWebsiteId())
+            || !$order->getData('customer_is_club_member')
+        ){
+            return $this;
+        }
+
+        // Check for club item. We don't give credit for allowing credit.
         if ($order->isNominal()) {
             foreach ($order->getAllVisibleItems() as $item) {
                 $productModel = Mage::getModel('catalog/product')->load($item->getProductId());
@@ -64,15 +72,8 @@ class Crown_Club_Model_Observer {
             }
         }
 
-        if ($order->getCustomerIsGuest()
-            || !Mage::helper('enterprise_reward')->isEnabledOnFront($order->getStore()->getWebsiteId())
-            || !$order->getData('customer_is_club_member')
-        ){
-            return $this;
-        }
-
-        // Give credit where credit is due
-        if ($order->getCustomerId() && $this->_isOrderPaidNow($order)) {
+        // Give credit where credit is due, if it's due that is...
+        if ($order->getCustomerId() && Mage_Sales_Model_Order::STATE_COMPLETE == $order->getState()) {
             /* @var $reward Enterprise_Reward_Model_Reward */
             $reward = Mage::getModel('enterprise_reward/reward')
                 ->setActionEntity($order)
@@ -104,26 +105,5 @@ class Crown_Club_Model_Observer {
         }
 
         return;
-    }
-
-    /**
-     * Check if order is paid exactly now
-     * If order was paid before Rewards were enabled, reward points should not be added
-     *
-     * @since 0.7.3
-     * @param Mage_Sales_Model_Order $order
-     * @return bool
-     */
-    protected function _isOrderPaidNow($order)
-    {
-        $isOrderPaid = (float)$order->getBaseTotalPaid() > 0
-            && ($order->getBaseGrandTotal() - $order->getBaseSubtotalCanceled() - $order->getBaseTotalPaid()) < 0.0001;
-
-        if (!$order->getOrigData('base_grand_total')) {//New order with "Sale" payment action
-            return $isOrderPaid;
-        }
-
-        return $isOrderPaid && ($order->getOrigData('base_grand_total') - $order->getOrigData('base_subtotal_canceled')
-            - $order->getOrigData('base_total_paid')) >= 0.0001;
     }
 }
