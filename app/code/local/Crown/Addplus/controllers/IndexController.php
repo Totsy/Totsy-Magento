c66f84986add0b0bf0f3290b22e12017da339d97
Trevor Bonsell <tbonsell@crownpartners.com>
2013-01-25 17:43:33 -0500

    Fixed addproduct module to throw an error if items are already in cart

--- app/code/local/Crown/Addplus/controllers/IndexController.php
+++ app/code/local/Crown/Addplus/controllers/IndexController.php
@@ -7,18 +7,20 @@
 
 class Crown_Addplus_IndexController extends Mage_Core_Controller_Front_Action {
     public function indexAction() {
-        // set the qty and the TotsyPLUS product ID by pulling it from store_config
-        $params = array(
-            'product' => Mage::getStoreConfig('Crown_Club/clubgeneral/club_product_id'),
-            'qty' => 1,
-        );
-        $cart = Mage::getSingleton('checkout/cart');
-        $product = new Mage_Catalog_Model_Product();
-        $product->load($params['product']);
-        $cart->addProduct($product, $params);
-        $cart->save();
-        Mage::getSingleton('checkout/session')->setCartWasUpdated(true);
-
+        if(Mage::helper('checkout/cart')->getCart()->getItemsCount() >= 1) {
+            Mage::getSingleton('core/session')->addError('In order to sign up for TotsyPLUS, please remove all existing items from your cart.');
+        } else {
+            // set the qty and the TotsyPLUS product ID by pulling it from store_config
+            $params = array(
+                'qty' => 1
+            );
+            $cart = Mage::getSingleton('checkout/cart');
+            $product = new Mage_Catalog_Model_Product();
+            $product->load(Mage::getStoreConfig('Crown_Club/clubgeneral/club_product_id'));
+            $cart->addProduct($product, $params);
+            $cart->save();
+            Mage::getSingleton('checkout/session')->setCartWasUpdated(true);
+        }
         $this->_redirect('checkout/cart');
     }
 }
