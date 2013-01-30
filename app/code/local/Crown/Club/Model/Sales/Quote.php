54941f77d488bb8b230542fd875ebfa1ce56ed88
Chris Davidowski <cdavidowski@crownpartners.com>
2013-01-30 04:58:16 +0000

    fixed plus membership activation on purchase

--- app/code/local/Crown/Club/Model/Sales/Quote.php
+++ app/code/local/Crown/Club/Model/Sales/Quote.php
@@ -1,33 +1,35 @@
 <?php
 // Mage_Sales_Model_Quote
-class Crown_Club_Model_Sales_Quote extends Harapartners_Service_Model_Rewrite_Sales_Quote {
-	
-	/**
-	 * Create recurring payment profiles basing on the current items
-	 *
-	 * @return array
-	 */
-	public function prepareRecurringPaymentProfiles() {
-		if (! $this->getTotalsCollectedFlag ()) {
-			// Whoops! Make sure nominal totals must be calculated here.
-			throw new Exception ( 'Quote totals must be collected before this operation.' );
-		}
-		
-		$result = array ();
-		foreach ( $this->getAllVisibleItems () as $item ) {
-			$product = $item->getProduct ();
-			if (is_object ( $product ) && ($product->isRecurring ()) && $profile = Mage::getModel ( 'sales/recurring_profile' )->importProduct ( $product )) {
-				$profile->importQuote ( $this );
-				$profile->importQuoteItem ( $item );
-				$result [] = $profile;
-				$productModel = Mage::getModel('catalog/product')->load($product->getEntityId());
-				if ( $productModel->getIsClubSubscription() ) {
-					$profile->setData('is_club_profile', true);
-				} else {
-					$profile->setData('is_club_profile', false);
-				}
-			}
-		}
-		return $result;
-	}
+class Crown_Club_Model_Sales_Quote extends Webshopapps_Shipdiscount_Sales_Model_Quote
+{
+
+    /**
+     * Create recurring payment profiles basing on the current items
+     *
+     * @return array
+     */
+    public function prepareRecurringPaymentProfiles()
+    {
+        if (!$this->getTotalsCollectedFlag()) {
+            // Whoops! Make sure nominal totals must be calculated here.
+            throw new Exception ('Quote totals must be collected before this operation.');
+        }
+
+        $result = array();
+        foreach ($this->getAllVisibleItems() as $item) {
+            $product = $item->getProduct();
+            if (is_object($product) && ($product->isRecurring()) && $profile = Mage::getModel('sales/recurring_profile')->importProduct($product)) {
+                $profile->importQuote($this);
+                $profile->importQuoteItem($item);
+                $result [] = $profile;
+                $productModel = Mage::getModel('catalog/product')->load($product->getEntityId());
+                if ($productModel->getIsClubSubscription()) {
+                    $profile->setData('is_club_profile', true);
+                } else {
+                    $profile->setData('is_club_profile', false);
+                }
+            }
+        }
+        return $result;
+    }
 }
