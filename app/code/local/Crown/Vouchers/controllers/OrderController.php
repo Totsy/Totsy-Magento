c9dd5b5c5a87b05a33b92cddd16e7902d1a15620
Tom Royer <troyer@totsy.com>
2013-03-18 19:56:51 +0000

    Revert "discount vault now uses ajax to update button availability"
    This reverts commit cde4d7b1886f0c6ff9b55bd3334b0b8ec049cdea.
    
    Conflicts:
    
    	app/code/local/Crown/Vouchers/controllers/OrderController.php

--- app/code/local/Crown/Vouchers/controllers/OrderController.php
+++ app/code/local/Crown/Vouchers/controllers/OrderController.php
@@ -42,34 +42,6 @@ class Crown_Vouchers_OrderController extends Mage_Core_Controller_Front_Action {
 		
 		$this->returnResult(array('success' => 'Your email has been sent!'));
 	}
-
-    /**
-     * AJAX request to check if voucher is available to user
-     *
-     * Created by Crown Partners
-     */
-    public function availableAction() {
-        $product_id = $this->getRequest()->getParam('id');
-
-        $customer_id = Mage::helper('customer')->getCustomer()->getId();
-
-        if(!$product_id) {
-            $this->returnResult(array('status' => 1));
-            return;
-        }
-
-        if(!$customer_id) {
-            $this->returnResult(array('status' => 2));
-            return;
-        }
-
-        if(Mage::helper('vouchers')->hasAssociation($customer_id, $product_id)) {
-            $this->returnResult(array('status' => 3));
-            return;
-        }
-
-        $this->returnResult(array('status' => 0));
-    }
 	
 	private function returnResult($result) {
 		$this->getResponse()->setBody(Mage::helper('core')->jsonEncode($result));
