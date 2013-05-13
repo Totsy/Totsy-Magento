e27186a49b1cdd6f5f022d92698a5e96bfba05b4
lindaliu <lindaliu@objectivasoftware.com>
2013-05-13 10:58:33 +0000

    fix bug MGN-1254

---
+++ app/code/local/Totsy/SalesRule/Model/Validator.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Magento Enterprise Edition
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Magento Enterprise Edition License
+ * that is bundled with this package in the file LICENSE_EE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.magentocommerce.com/license/enterprise-edition
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Totsy
+ * @package     Totsy_SalesRule
+ * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://www.magentocommerce.com/license/enterprise-edition
+ */
+
+
+/**
+ * SalesRule Validator Model
+ *
+ * Allows dispatching before and after events for each controller action
+ *
+ * @category   Totsy
+ * @package    Totsy_SalesRule
+ * @author     Linda Liu <lindaliu@objectivasoftware.com>
+ * 
+ */
+class Totsy_SalesRule_Model_Validator extends Mage_SalesRule_Model_Validator
+{
+        /**
+     * Get address object which can be used for discount calculation
+     *
+     * @param   Mage_Sales_Model_Quote_Item_Abstract $item
+     * @return  Mage_Sales_Model_Quote_Address
+     */
+        public function _getAddress(Mage_Sales_Model_Quote_Item_Abstract $item)
+    {
+        if ($item instanceof Mage_Sales_Model_Quote_Address_Item) {
+            $address = $item->getAddress();
+        } elseif ($item->getQuote()->isVirtual()) {
+            $address = $item->getQuote()->getBillingAddress();
+        } else {
+            $address = $item->getQuote()->getShippingAddress();
+        }
+        return $address;
+    }
+}
